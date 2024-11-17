e411cf53a1e02a8df4fa55923f1e866e3fd609bf
Lima-44 <lima44zero13@gmail.com>
2024-11-17 10:43:10 -0300

    Add respositório App

--- MauiAppHotel/Views/ContratacaoHopedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHopedagem.xaml.cs
@@ -2,8 +2,27 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHopedagem : ContentPage
 {
+	App PropriedadesApp;
+
 	public ContratacaoHopedagem()
 	{
 		InitializeComponent();
+
+		PropriedadesApp = (App)Application.Current;
+
+		pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Navigation.PushAsync(new HospedagemContratada());
+
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
 	}
 }
